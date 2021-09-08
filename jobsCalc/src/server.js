9155b3d4b1da80138af515b5cdc2e357b56e03b8
pallomagualter <pallomagualter@seduc.pi.gov.br>
2021-09-08 10:55:20 -0300

    jobsCalc

--- jobsCalc/src/server.js
+++ jobsCalc/src/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const server = express();
 
+//habilitar arquivos statics
+server.use(express.static("public"))
+
+//request, response
 server.get('/', (request, response) => {
-    return response.send('Hi!')
+    return response.sendFile(__dirname + "/views/index.html")
 });
 
 server.listen(3000, () => console.log('server running... '));
